f592e74de770a1a3a8323c97d9a674cddbaca1b8
Hanna-SS <aninha.souza123@gmail.com>
2026-03-26 17:36:50 -0300

    fix: move SSL bypass to hooks.server.ts for earlier execution

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,8 @@
 import { initCron } from '$lib/services/cronManager';
 
+// Force bypass SSL globally for production environments like SquareCloud
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 let cronInitialized = false;
 
 /** @type {import('@sveltejs/kit').Handle} */
